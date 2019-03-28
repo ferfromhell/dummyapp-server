6e429f67b1b3127776b7d935e359f8c37e14956a
Fernando Lujan <fernando@Fernandos-MacBook-Pro.local>
2019-03-27 20:06:19 -0700

    remove cors

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+//CORS
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+app.get('/test', function (req, res) {
+  res.json({text: "connected to server"});
 });
 
 app.listen(5000, function () {
